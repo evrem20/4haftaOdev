20b279c26d8e8d7af27dc4e1b71e92591d177a19
merve çalışır <m-r-v-c-96@hotmail.com>
2021-01-27 00:21:03 +0300

    Add project files.

---
+++ 4haftaOdev/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4haftaOdev
+{
+    class MyDictionary<Tkey, Tvalue>  //Ben mydictonaryimde Tkey ve Tvalue ile çalışıcam anlamına geliyor.
+
+    {
+        Tkey[] tkeys;             //
+        Tvalue[] tvalues;
+
+        public MyDictionary()    //Burası constructordır. Otomatik çalışır.
+        {
+            tkeys = new Tkey[0];         //0 elemanlı bir dizi oluşturduk.
+            tvalues = new Tvalue[0];
+
+        }
+
+        public void Add(Tkey anahtar, Tvalue deger)
+        {
+            Tkey[] tempArray = tkeys;     //Geçiçi dizinin referansı tkeysin referansı. tkeysin reeransını tutuyor.
+            Tvalue[] tempArray2 = tvalues;  //Önceki referansa bağlanan elemanlar silinmesin diye bu referansları geçici diziye tutturuyoruz.
+
+            tkeys = new Tkey[tkeys.Length + 1];           //Burada newlediğimiz için değerler,referans numarası uçuyor o yüzden bu referansı birrine tutturmak için bir yukarıdaki işlem yapılır.
+            tvalues = new Tvalue[tvalues.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                tkeys[i] = tempArray[i];
+            }
+
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                tvalues[i] = tempArray2[i];
+
+            }
+
+            tkeys[tkeys.Length - 1] = anahtar;
+            tvalues[tvalues.Length - 1] = deger;
+        }
+
+
+        public Tkey[] No
+        {
+            get { return tkeys; }
+
+        }
+        public Tvalue[] Isim
+        {
+            get { return tvalues; }
+        }
+
+    
+    }
+}
---
+++ 4haftaOdev/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4haftaOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+            ogrenci.Add(86, "Eren");
+            ogrenci.Add(20, "Merve");
+            ogrenci.Add(34, "Kerem");
+            ogrenci.Add(36, "Aslı");
+            ogrenci.Add(59, "Ayşe");
+            ogrenci.Add(44, "Emre");
+
+            Console.WriteLine("Öğrenci No" + ":" + ogrenci.No[0] + " " + "Öğrenci İsmi" + ":" + ogrenci.Isim[0]);
+            Console.WriteLine("Öğrenci No" + ":" + ogrenci.No[1] + " " + "Öğrenci İsmi" + ":" + ogrenci.Isim[1]);
+            Console.WriteLine("Öğrenci No" + ":" + ogrenci.No[2] + " " + "Öğrenci İsmi" + ":" + ogrenci.Isim[2]);
+            Console.WriteLine("Öğrenci No" + ":" + ogrenci.No[3] + " " + "Öğrenci İsmi" + ":" + ogrenci.Isim[3]);
+            Console.WriteLine("Öğrenci No" + ":" + ogrenci.No[4] + " " + "Öğrenci İsmi" + ":" + ogrenci.Isim[4]);
+            Console.WriteLine("Öğrenci No" + ":" + ogrenci.No[5] + " " + "Öğrenci İsmi" + ":" + ogrenci.Isim[5]);
+        }
+    }
+}
